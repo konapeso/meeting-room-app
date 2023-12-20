a9cc050632f21eb9ac0a1ee163773e9e4715eea0
konapeso <n2kgt1@gmail.com>
2023-12-20 19:38:03 +0900

    ヘッダー追加

---
+++ src/components/Header.tsx
@@ -0,0 +1,34 @@
+// components/Header.js
+
+import Link from "next/link";
+import { useAuth } from "../context/AuthContext"; // useAuthフックをインポート
+
+const Header = () => {
+  const auth = useAuth(); // AuthコンテキストからuseAuthフックを取得
+
+  const handleLogout = () => {
+    // ログアウト処理
+    auth.logout();
+  };
+
+  return (
+    <header className="bg-blue-500 p-4">
+      <nav className="flex justify-between items-center">
+        <div>
+          <Link href="/rooms" className="text-white text-xl font-semibold">
+            会議室予約
+          </Link>
+        </div>
+        <div>
+          {auth.isLoggedIn ? ( // ログイン状態に応じてログアウトボタンを表示
+            <button onClick={auth.logout} className="text-white">
+              ログアウト
+            </button>
+          ) : null}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
