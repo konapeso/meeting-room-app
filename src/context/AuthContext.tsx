fe858a1c405de6b41c17975e14a4c26f4790892b
konapeso <n2kgt1@gmail.com>
2023-12-20 14:08:03 +0900

    ログイン機能の実装

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,58 @@
+// context/AuthContext.tsx
+import {
+  createContext,
+  useState,
+  useContext,
+  useEffect,
+  ReactNode,
+} from "react";
+import { useRouter } from "next/router";
+
+interface AuthContextType {
+  isLoggedIn: boolean;
+  login: (token: string) => void;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | null>(null);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    // トークンをチェックしてログイン状態を更新
+    const token = localStorage.getItem("token");
+    if (token) {
+      setIsLoggedIn(true);
+    } else {
+      setIsLoggedIn(false);
+    }
+  }, []);
+
+  const login = (token: string) => {
+    localStorage.setItem("token", token);
+    setIsLoggedIn(true);
+    router.push("/rooms"); // ログイン後のリダイレクト先
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setIsLoggedIn(false);
+    router.push("/login");
+  };
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,34 +1,59 @@
-import { useState } from "react";
+import { useState, FormEvent } from "react";
+import { useAuth } from "../context/AuthContext";
 
-function LoginPage() {
-  const [email, setEmail] = useState("");
+const LoginPage = () => {
+  const [userId, setUserId] = useState("");
   const [password, setPassword] = useState("");
+  const auth = useAuth();
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = async (event: FormEvent) => {
     event.preventDefault();
-    // ログイン処理
+    // ユーザーIDを整数に変換
+    const userIdInt = parseInt(userId, 10);
+    // ログイン処理（FastAPIサーバーとの通信）
+    const response = await fetch("http://localhost:8000/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      // フィールド名をuser_idに変更
+      body: JSON.stringify({ user_id: userIdInt, password }),
+    });
+    if (response.ok) {
+      const { token } = await response.json();
+      auth.login(token);
+    } else {
+      console.error("ログイン失敗");
+    }
   };
 
   return (
-    <div>
-      <h1>ログイン</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="メールアドレス"
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="パスワード"
-        />
-        <button type="submit">ログイン</button>
-      </form>
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="p-6 bg-white rounded shadow-md">
+        <h1 className="text-lg font-bold mb-4">ログイン</h1>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <input
+            type="id"
+            value={userId}
+            onChange={(e) => setUserId(e.target.value)}
+            placeholder="ユーザーID"
+            className="w-full p-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="パスワード"
+            className="w-full p-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
+          />
+          <button
+            type="submit"
+            className="w-full p-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          >
+            ログイン
+          </button>
+        </form>
+      </div>
     </div>
   );
-}
+};
 
 export default LoginPage;
