e9e8633c39d85c71ca6db1ca5582638cf5cf09fd
konapeso <n2kgt1@gmail.com>
2023-12-21 21:17:13 +0900

    完了画面のスタイル

--- src/pages/reserved.tsx
+++ src/pages/reserved.tsx
@@ -1,8 +1,25 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 function ReservedPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    // 3秒後にホームページにリダイレクト
+    const timer = setTimeout(() => {
+      router.push("/");
+    }, 3000);
+
+    // コンポーネントのアンマウント時にタイマーをクリア
+    return () => clearTimeout(timer);
+  }, [router]);
+
   return (
-    <div>
-      <h1>予約完了</h1>
-      <p>会議室の予約が完了しました。</p>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <div className="bg-white p-8 rounded shadow-md text-center">
+        <h1 className="text-2xl font-bold text-blue-500 mb-4">予約完了</h1>
+        <p className="text-gray-700">会議室の予約が完了しました。</p>
+      </div>
     </div>
   );
 }
